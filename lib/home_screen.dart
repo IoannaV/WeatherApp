2628df8026fc6836553e927befd5ac67d1d5eb95
ioanna <vasilyeva.io81@gmail.com>
2021-10-25 21:33:36 +0300

    Add dropdown to appbar

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Weather app")),
-      body: const Center(child: Text("app")),
+      appBar: AppBar(
+        title: const Text("Weather app"),
+        actions: [WeatherFilterWidget()],
+      ),
+      body: Center(child: Text("app")),
+    );
+  }
+}
+
+class WeatherFilterWidget extends StatefulWidget {
+  WeatherFilterWidget({Key? key}) : super(key: key);
+
+  @override
+  _WeatherFilterWidgetState createState() => _WeatherFilterWidgetState();
+}
+
+class _WeatherFilterWidgetState extends State<WeatherFilterWidget> {
+  String _value = 'Hours';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: _value,
+      onChanged: (String? newValue) {
+        setState(() {
+          _value = newValue!;
+        });
+      },
+      items: <String>['Hours', 'Days']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
     );
   }
 }
